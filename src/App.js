4a1750280abb4dba37b63001493391d6c2aa198f
Unknown <bnmdude@gmail.com>
2018-04-01 14:27:29 +0200

    basic structure

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+
+import './assets/css/default.min.css';
+
+const App = ({location, dispatch, history}) => (
+    <div className="reactBody">
+        <div className='content'>
+        </div>
+    </div>
+);
+
+App.propTypes = {
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired
+    }).isRequired,
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+export default connect(mapStateToProps)(App);
